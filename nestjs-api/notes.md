1ada815f186da4c93eb67bf7153a4029c1911452
felipematthew <felipematthewnascimento@gmail.com>
2024-02-08 19:16:13 -0300

    deleting to reup

--- nestjs-api/notes.md
+++
@@ -1,1 +0,0 @@
-https://media.discordapp.net/attachments/901313407083495446/1203389184036053032/image.png?ex=65d0ea83&is=65be7583&hm=6db119f1d8fd08c32aae933985bb0d3675303b9b879c499e7a95faa34cb66f30&=&format=webp&quality=lossless&width=1264&height=676
--- nestjs-api/src/products/dto/update-product.dto.ts
+++
@@ -1,6 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateProductDto } from './create-product.dto';
-
-// Procura apenas um campo
-// Tem o dto para que seja configurado  os campos obrigatórios ou não, e outros detalhes
-export class UpdateProductDto extends PartialType(CreateProductDto) {}
